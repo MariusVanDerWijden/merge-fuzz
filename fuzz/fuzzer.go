2cc50228a6384de145a3b8c484cc2715a0c4c05b
Marius van der Wijden <m.vanderwijden@live.de>
2021-09-16 14:31:07 +0200

    fuzz: use gofuzz

--- fuzz/fuzzer.go
+++ fuzz/fuzzer.go
@@ -2,25 +2,34 @@ package fuzz
 
 import (
 	"github.com/ethereum/go-ethereum/common"
+	fuzz "github.com/google/gofuzz"
 	"github.com/mariusvanderwijden/merge-fuzz/merge"
 )
 
 var engine merge.Engine
 
 func FuzzPreparePayload(input []byte) int {
+	fuzzer := fuzz.NewFromGoFuzz(input)
 	var (
 		parentHash   common.Hash
 		timestamp    uint64
 		random       [32]byte
 		feeRecipient common.Address
-		payload      uint64
+		payloadID    uint64
 	)
-	engine.PreparePayload(parentHash, timestamp, random, feeRecipient, payload)
+	fuzzer.Fuzz(&parentHash)
+	fuzzer.Fuzz(&timestamp)
+	fuzzer.Fuzz(&random)
+	fuzzer.Fuzz(&feeRecipient)
+	fuzzer.Fuzz(&payloadID)
+	engine.PreparePayload(parentHash, timestamp, random, feeRecipient, payloadID)
 	return 0
 }
 
 func FuzzGetPayload(input []byte) int {
+	fuzzer := fuzz.NewFromGoFuzz(input)
 	var payloadID uint64
+	fuzzer.Fuzz(&payloadID)
 	payload, err := engine.GetPayload(payloadID)
 	if err != nil {
 		return 0
@@ -30,7 +39,8 @@ func FuzzGetPayload(input []byte) int {
 }
 
 func FuzzExecutePayload(input []byte) int {
-	var payload merge.ExecutionPayload
+	fuzzer := fuzz.NewFromGoFuzz(input)
+	payload := fillExecPayload(fuzzer)
 	hash, status := engine.ExecutePayload(payload)
 	if status == merge.VALID || status == merge.KNOWN {
 		return 1
@@ -40,7 +50,9 @@ func FuzzExecutePayload(input []byte) int {
 }
 
 func FuzzConsensusValidated(input []byte) int {
+	fuzzer := fuzz.NewFromGoFuzz(input)
 	var blockhash common.Hash
+	fuzzer.Fuzz(&blockhash)
 	status := engine.ConsensusValidated(blockhash)
 	if status == merge.VALID || status == merge.KNOWN {
 		return 1
@@ -49,14 +61,28 @@ func FuzzConsensusValidated(input []byte) int {
 }
 
 func FuzzForkchoiceUpdated(input []byte) int {
+	fuzzer := fuzz.NewFromGoFuzz(input)
 	var (
 		headBlockHash      common.Hash
 		finalizedBlockHash common.Hash
 		confirmedBlockHash common.Hash
 	)
+	fuzzer.Fuzz(&headBlockHash)
+	fuzzer.Fuzz(&finalizedBlockHash)
+	fuzzer.Fuzz(&confirmedBlockHash)
 	err := engine.ForkchoiceUpdated(headBlockHash, finalizedBlockHash, confirmedBlockHash)
 	if err == nil {
 		return 1
 	}
+	return 0
+}
+
+func fillExecPayload(fuzzer *fuzz.Fuzzer) merge.ExecutionPayload {
+	var payload merge.ExecutionPayload
+	fuzzer.Fuzz(&payload)
+	return payload
+}
+
+func FuzzInteraction(input []byte) int {
 	return 0
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mariusvanderwijden/merge-fuzz
 
 go 1.16
 
-require github.com/ethereum/go-ethereum v1.10.8 // indirect
+require (
+	github.com/ethereum/go-ethereum v1.10.8 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+)
